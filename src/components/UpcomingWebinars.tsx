688ae1c90e6df80478097507485c3614b14d8b52
SattvaDoshi <sattva103@gmail.com>
2024-05-22 20:52:29 +0530

    Deployment

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -1,53 +1,7 @@
 'use client'
 import Link from "next/link"
-import { HoverEffect } from "./ui/card-hover-effect";
 
 function UpcomingWebinars() {
-
-  // const featuredWebinars = [
-  //   {
-  //     title: 'Understanding Music Theory',
-  //     description:
-  //       'Dive deep into the fundamentals of music theory and enhance your musical skills.',
-  //     slug: 'understanding-music-theory',
-  //     isFeatured: true,
-  //   },
-  //   {
-  //     title: 'The Art of Songwriting',
-  //     description:
-  //       'Learn the craft of songwriting from experienced musicians and songwriters.',
-  //     slug: 'the-art-of-songwriting',
-  //     isFeatured: true,
-  //   },
-  //   {
-  //     title: 'Mastering Your Instrument',
-  //     description:
-  //       'Advanced techniques to master your musical instrument of choice.',
-  //     slug: 'mastering-your-instrument',
-  //     isFeatured: true,
-  //   },
-  //   {
-  //     title: 'Music Production Essentials',
-  //     description:
-  //       'Get started with music production with this comprehensive overview.',
-  //     slug: 'music-production-essentials',
-  //     isFeatured: true,
-  //   },
-  //   {
-  //     title: 'Live Performance Techniques',
-  //     description:
-  //       'Enhance your live performance skills with expert tips and strategies.',
-  //     slug: 'live-performance-techniques',
-  //     isFeatured: true,
-  //   },
-  //   {
-  //     title: 'Digital Music Marketing',
-  //     description:
-  //       'Learn how to promote your music effectively in the digital age.',
-  //     slug: 'digital-music-marketing',
-  //     isFeatured: true,
-  //   },
-  // ];
 
   return (
     <div className="pt-5 bg-gray-900" id="pricing">
